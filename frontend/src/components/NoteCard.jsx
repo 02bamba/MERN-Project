eca197da59f04a2decf798963606bbfdefea19a2
02bamba <cabkebe02@gmail.com>
2025-09-19 17:33:40 +0000

    create Detail page, and others fonctionality

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,10 +1,26 @@
 import { Link } from 'react-router'
 import { PenSquareIcon, Trash2Icon } from "lucide-react";
 import { formatDate } from "../lib/utils";
+import api from '../lib/axios';
+import toast from 'react-hot-toast';
 
-function NoteCard({note}) {
+function NoteCard({note, setNotes}) {
 
-    const handleDelete = () => {};
+    const handleDelete = async (e, id) => {
+      e.preventDefault();
+
+      if(!window.confirm("Are tou sure to delete this note ?")) return;
+
+      try {
+        await api.delete(`/notes/${id}`);
+
+        setNotes((prev) => prev.filter(note => note._id !== id))
+        toast.success("Note deleted succcessfully 👍")
+      } catch (error) {
+        console.log("Error while deleting the note: ", error);
+        toast.error("Failed to delete the note.");
+      }
+    };
   return (
     <Link to={`/note/${note._id}`}
     className="card bg-base-100 hover:shadow-lg transition-all duration-200 
@@ -15,7 +31,7 @@ function NoteCard({note}) {
             <p className="text-base-content/70 line-clamp-3">{note.content}</p>
         <div className="card-actions justify-between items-center mt-4">
             <span className="text-sm text-base-content/60">
-            {formatDate(new Date(note.createdAt))}
+            {formatDate(new Date((note.createdAt)))}
           </span>
           <div className="flex items-center gap-1">
             <PenSquareIcon className="size-4" />
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import RateLimitedUi from '../components/RateLimitedUi';
-import axios from "axios";
 import toast from "react-hot-toast";
 import NoteCard from '../components/NoteCard';
+import api from '../lib/axios';
+import NotesNotFound from '../components/NotesNotFound';
 
 function HomePage() {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -14,7 +15,7 @@ function HomePage() {
   useEffect(() => {
     const fetchNotes = async () => {
     try {
-      const res = await axios.get("http://localhost:5000/api/notes")
+      const res = await api.get("/notes")
       setNotes(res.data)
       setIsRateLimited(false)
     } catch (error) {
@@ -41,11 +42,13 @@ function HomePage() {
       <div className='max-w-7xl mx-auto p-4 mt-6'>
         {loading && <div className='text-center text-primary py-10'>loading notes ...</div>}
 
+        {notes.length === 0 && !isRateLimited && <NotesNotFound/>}
+        
         {notes.length > 0 && !isRateLimited  && (
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
             {
               notes.map((note) =>(
-                <NoteCard key={note._id} note={note}/>
+                <NoteCard key={note._id} note={note} setNotes={setNotes}/>
               ))
             }
           </div>
